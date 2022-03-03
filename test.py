a761f6bb25a8f576bc191e7022ec24a991ad62c9
chunshan feng <32756270+fengchunshan@users.noreply.github.com>
2022-03-02 19:03:12 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-#Collecting The Input As A Variable:#
-name = input('Please enter your name: ')
-#Printing The Variable:#
-print(name)
-#Checking The Variable And Printing Accordingly:#
-if name == 'Joe':
-  print('Joe Mama')
+print('adb')
