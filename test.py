093ec0d5a188c381116599a4c4f0e70cf450d960
chunshan feng <32756270+fengchunshan@users.noreply.github.com>
2022-03-02 18:46:43 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+#Collecting The Input As A Variable:#
+name = input('Please enter your name: ')
+#Printing The Variable:#
+print(name)
+#Checking The Variable And Printing Accordingly:#
+if name == 'Joe':
+  print('Joe Mama')
